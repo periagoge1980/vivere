83745b79c3d39ec34bbb4e359ec74cf251b09be4
periagoge1980 <123590443+periagoge1980@users.noreply.github.com>
2024-07-23 15:57:06 -0400

    Update resources.js

--- resources.js
+++ resources.js
@@ -4,12 +4,12 @@ const resources = [
   {
     "type": "Alcohol",
     "name": "Resource Name 1",
-    "address": "123 Street, City, Quebec",
-    "postal_code": "G1K 3H6",
+    "address": "8666 Avenue Henri-Julie, Montreal, Quebec",
+    "postal_code": "H2P 2J7",
     "online_url": "http://resource-website1.com",
     "phone": "123-456-7890",
-    "latitude": 46.8139,
-    "longitude": -71.2082
+    "latitude": 45.5434,
+    "longitude": 73.6378
   },
   {
     "type": "Drugs",
