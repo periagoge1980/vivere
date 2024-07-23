ac711909207198469b959a8c593b30148e2e0572
periagoge1980 <123590443+periagoge1980@users.noreply.github.com>
2024-07-23 15:41:47 -0400

    Update getApiKey.js

--- netlify/functions/getApiKey.js
+++ netlify/functions/getApiKey.js
@@ -1,1 +1,8 @@
+// netlify/functions/getApiKey.js
 
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ key: process.env.GOOGLE_MAPS_API_KEY }),
+  };
+};
