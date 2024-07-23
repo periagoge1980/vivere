cac559824e8bc1155a7423545ad28e92383f2f05
periagoge1980 <123590443+periagoge1980@users.noreply.github.com>
2024-07-23 15:18:39 -0400

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,25 @@
+// resources.js
+
+const resources = [
+  {
+    "type": "Alcohol",
+    "name": "Resource Name 1",
+    "address": "123 Street, City, Quebec",
+    "postal_code": "G1K 3H6",
+    "online_url": "http://resource-website1.com",
+    "phone": "123-456-7890",
+    "latitude": 46.8139,
+    "longitude": -71.2082
+  },
+  {
+    "type": "Drugs",
+    "name": "Resource Name 2",
+    "address": "456 Avenue, City, Quebec",
+    "postal_code": "H2Y 1C6",
+    "online_url": "http://resource-website2.com",
+    "phone": "987-654-3210",
+    "latitude": 45.5017,
+    "longitude": -73.5673
+  }
+  // Add more resources as needed
+];
