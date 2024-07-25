124eebf30541583c4de8eb113826b1aa80c0dc1e
periagoge1980 <123590443+periagoge1980@users.noreply.github.com>
2024-07-24 21:35:49 -0400

    Update script.js
    Trying to fix google map

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let map;
 let geocoder;
 let resources = []; // This will be populated by the JSON data
 
+// Initialize the map
 function initMap() {
   console.log("Initializing map...");
   geocoder = new google.maps.Geocoder();
@@ -91,7 +92,7 @@ function displayResources(resources) {
 }
 
 // Load resources from JSON (resources.js is included in the HTML)
-async function loadResources() {
+function loadResources() {
   try {
     // resources is already defined in resources.js
     console.log("Resources loaded:", resources);
@@ -102,13 +103,17 @@ async function loadResources() {
 
 // Load Google Maps API with the key fetched from Netlify function
 async function loadGoogleMaps() {
-  const response = await fetch('/.netlify/functions/getApiKey');
-  const data = await response.json();
-  const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${data.key}&callback=initMap`;
-  script.async = true;
-  script.defer = true;
-  document.head.appendChild(script);
+  try {
+    const response = await fetch('/.netlify/functions/getApiKey');
+    const data = await response.json();
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${data.key}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  } catch (error) {
+    console.error("Error loading Google Maps:", error);
+  }
 }
 
 // Initial load
