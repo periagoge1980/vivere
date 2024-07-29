be5eb6ee743945b461c6947d4da59d4b79f050ae
periagoge1980 <123590443+periagoge1980@users.noreply.github.com>
2024-07-29 14:32:10 -0400

    Update resources.js

--- resources.js
+++ resources.js
@@ -1,206 +1,218 @@
-// resources.js
 function initializePage() {
-const resources = 
-[
-  {
-    "data_source":"https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=190",
-    "name":"Mission Bon Accueil - Résidences Bon accueil",
-    "phone":"514 523-5288",
-    "address":"4755A, rue Acorn, Montréal (Québec) H4C 3L6",
-    "website":"https://missionbonaccueil.com/services-fr/soins-et-soutien/",
-    "services":null,
-    "clientele":"Clientèle : Hommes (18 ans et plus), personnes judiciarisées, personnes en traitement de substitution",
-    "capacity":"Capacité: 28 personnes",
-    "dependency_category":["Drugs", "Alcohol"],
-    "email":null,
-    "program_duration":"Durée du programme : 180 jours",
-    "approach":"Approche(s) préconisée(s) : Approche de type biopsychosocial (modèle Minnesota), approche cognitive et entretien motivationnel",
-    "postal_code":"H4C 3L6",
-    "longitude": -73.5789,
-    "latitude": 45.4712
-  },
-  {
-    "data_source":"https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=146",
-    "name":"Maison L’Exode – Pavillon André-Dumont",
-    "phone":"514 255-7727",
-    "address":"2590, avenue Létourneux, Montréal (Québec) H1V 2P4",
-    "website":"www.maison-exode.org",
-    "services":null,
-    "clientele":null,
-    "capacity":"Clientèle : Hommes et femmes (18 ans et plus), personnes judiciarisées",
-    "dependency_category":["Drugs", "Alcohol"],
-    "email":null,
-    "program_duration":"Capacité: 20 personnes",
-    "approach":"Durée du programme : 90 à 365 jours",
-    "postal_code":"H1V 2P4",
-    "longitude": -73.5531,
-    "latitude": 45.5619
-  },
-  {
-    "data_source":"https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=277",
-    "name":"Maison L’Exode – Pavillon Alpha",
-    "phone":"514 508-6900",
-    "address":"6400, rue Clark, Montréal (Québec) H2S 3E6",
-    "website":"www.maison-exode.org",
-    "services":"Aide et soutien à la récupération à la suite d'une intoxication (dégrisement)",
-    "clientele":"Clientèle : Hommes et femmes (18 ans et plus), personnes présentant à la fois une dépendance et un trouble de santé mentale",
-    "capacity":"Capacité: 12 places",
-    "dependency_category":["Drugs", "Alcohol", "Gambling"],
-    "email":null,
-    "program_duration":"Durée du programme : Maximum 3 jours",
-    "approach":"Approche(s) préconisée(s) : Réduction des méfaits",
-    "postal_code":"H2S 3E6",
-    "longitude": -73.6175,
-    "latitude": 45.5355
-  },
-  {
-    "data_source":"https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=151",
-    "name":"Le Grand Chemin (Montréal)",
-    "phone":"514 381-1218",
-    "address":"7070, boulevard Perras, Montréal (Québec) H1E 1A4",
-    "website":"www.legrandchemin.qc.ca",
-    "services":"Thérapie  en toxicomanie (individuelle ou de groupe)",
-    "clientele":null,
-    "capacity":"Clientèle : Jeunes de 12 à 17 ans",
-    "dependency_category":["Drugs", "Alcohol", "Gambling"],
-    "email":null,
-    "program_duration":"Capacité: 12 personnes",
-    "approach":"Durée du programme : 56 à 70 jours",
-    "postal_code":"H1E 1A4",
-    "longitude": -73.6235,
-    "latitude": 45.6481
-  },
-  {
-    "data_source": "https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=190",
-    "name": "Villa Ignatia inc.",
-    "phone": "418 849-6534",
-    "address": "2205, rue du Beau-Site, Saint-Emile, Quebec, G3E 1N1",
-    "website": "http://www.villaignatia.com/",
-    "services": "Aide et soutien à la désintoxication",
-    "clientele": "Clientèle: Hommes et femmes (18 ans et plus), personnes judiciarisées, personnes en traitement de substitution",
-    "capacity": "Capacité: 26 personnes",
-    "dependency_category":["Drugs", "Alcohol"],
-    "email": null,
-    "program_duration": "Durée du programme: 28 jours",
-    "approach": "Approche(s) préconisée(s): Humaniste et psychodynamique",
-    "latitude": 46.890,
-    "longitude": -71.219,
-    "postal_code": "G3E 1N1"
-  },
-  {
-    "data_source": "https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=191",
-    "name": "Résidence Le Portail",
-    "phone": "418 878-2867",
-    "address": "1240, route de Fossambault, Saint-Augustin-de-Desmaures, Quebec, G3A 2H2",
-    "website": "http://www.therapieleportail.org/",
-    "services": "Thérapie en toxicomanie (individuelle ou de groupe)",
-    "clientele": "Clientèle: Femmes (18 ans et plus), personnes judiciarisées, personnes en traitement de substitution",
-    "capacity": "Capacité: 15 personnes",
-    "dependency_category":["Drugs", "Alcohol"],    
-    "email": null,
-    "program_duration": "Durée du programme: 28 jours",
-    "approach": "Approche(s) préconisée(s): Cognitivo-comportementale",
-    "latitude": 46.756,
-    "longitude": -71.457,
-    "postal_code": "G3A 2H2"
-  },
-  {
-    "data_source": "https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=192",
-    "name": "Maison d'aide Villa Saint-Léonard",
-    "phone": "418 337-8808 ou 1 800 550-8808",
-    "address": "1333, Grand Rang, Saint-Raymond, Quebec, G3L 4K1",
-    "website": "http://www.villa-st-leonard.org",
-    "services": "Thérapie en toxicomanie (individuelle ou de groupe)",
-    "clientele": "Clientèle: Hommes et femmes (18 ans et plus), personnes judiciarisées, personnes sous traitement de substitution, personnes qui présentent à la fois une dépendance et un trouble de santé mentale",
-    "capacity": "Capacité: 20 personnes",
-    "dependency_category":["Drugs", "Alcohol", "Gambling"],
-    "email": null,
-    "program_duration": "Durée du programme: 28 jours",
-    "approach": "Approche(s) préconisée(s): Humaniste et psychodynamique",
-    "latitude": 46.893,
-    "longitude": -71.857,
-    "postal_code": "G3L 4K1"
-  },
-  {
-    "data_source": "https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=193",
-    "name": "Le Grand Chemin (Québec)",
-    "phone": "418 523-1218",
-    "address": "1, avenue du Sacré-Cœur, Québec, Quebec, G1N 2W1",
-    "website": "http://www.legrandchemin.qc.ca",
-    "services": "Thérapie en jeu pathologique",
-    "clientele": "Clientèle: Jeunes de 12 à 17 ans",
-    "capacity": "Capacité: 12 personnes",
-    "dependency_category":["Gambling"],
-    "email": null,
-    "program_duration": "Durée du programme: 56 à 70 jours",
-    "approach": "Approche(s) préconisée(s): Cognitivo-comportementale",
-    "latitude": 46.813,
-    "longitude": -71.219,
-    "postal_code": "G1N 2W1"
-  },
-  {
-    "data_source": "https://www.msss.gouv.qc.ca/repertoires/dependances/fiche_public.php?nofiche=194",
-    "name": "Centre de réadaptation en dépendance de Québec",
-    "phone": "418 663-5008",
-    "address": "2525, chemin de la Canardière, Québec, Quebec, G1J 2G3",
-    "website": "http://www.ciusss-capitalenationale.gouv.qc.ca/fr/crdq",
-    "services": "Aide et soutien à l'entourage",
-    "clientele": "Clientèle: Jeunes et adultes",
-    "capacity": null,
-    "dependency_category":["Drugs", "Alcohol"],
-    "email": null,
-    "program_duration": "Autres informations: Évaluation des conducteurs (SAAQ)",
-    "approach": "Approche(s) préconisée(s): Évaluation des conducteurs (SAAQ)",
-    "latitude": 46.847,
-    "longitude": -71.213,
-    "postal_code": "G1J 2G3"
-  }
-];
-// Main JavaScript file
+  const storedNotes = JSON.parse(localStorage.getItem('notes')) || {};
+  const storedCommitTime = localStorage.getItem('commitTime');
 
-function renderResourceList(resources) {
-  const resourceList = document.getElementById('resourceList');
-  resourceList.innerHTML = '';
-
-  resources.forEach((resource, index) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <h2>${resource.name}</h2>
-      <p>${resource.address}</p>
-      <button onclick="viewResourceDetail(${index})">View Details</button>
-    `;
-    resourceList.appendChild(listItem);
+  $('#dateForm').on('submit', function(e) {
+    e.preventDefault();
+    var selectedDate = $('#startDate').val();
+    if (selectedDate) {
+      const commitTime = new Date(); // Capture the exact time of commit
+      const selectedDateArray = selectedDate.split('-');
+      commitTime.setFullYear(
+        selectedDateArray[0],
+        selectedDateArray[1] - 1,
+        selectedDateArray[2]
+      );
+      localStorage.setItem('commitTime', commitTime.toISOString());
+      displayCalendar(selectedDate);
+      startTimer(commitTime);
+    }
   });
 
-  console.log("Resource list rendered with", resources.length, "items.");
+  if (storedCommitTime) {
+    const commitTime = new Date(storedCommitTime);
+    $('#startDate').val(moment(commitTime).format('YYYY-MM-DD'));
+    displayCalendar(moment(commitTime).format('YYYY-MM-DD'));
+    startTimer(commitTime);
+  } else {
+    displayCalendar(new Date());
+  }
+
+  function displayCalendar(date) {
+    $('#calendar').fullCalendar('destroy'); // Destroy any existing calendar
+    $('#calendar').fullCalendar({
+      defaultDate: date,
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      dayClick: function(date) {
+        const selectedDate = date.format('YYYY-MM-DD');
+        $('#note-popup').data('date', selectedDate).show();
+        displayNotes(selectedDate);
+      },
+      events: getStoredEvents()
+    });
+  }
+
+  function getStoredEvents() {
+    let events = [];
+    for (let date in storedNotes) {
+      storedNotes[date].forEach(note => {
+        events.push({
+          title: note.title,
+          start: date,
+          color: note.color
+        });
+      });
+    }
+    return events;
+  }
+
+  function displayNotes(date) {
+    const notesContainer = $('#notes-container');
+    notesContainer.empty(); // Clear any previous notes
+    if (storedNotes[date]) {
+      storedNotes[date].forEach(note => {
+        const noteBlock = `<div class="note-block" style="background-color: ${note.color};">${note.note}</div>`;
+        notesContainer.append(noteBlock);
+      });
+      $('#remove-note').show();
+    } else {
+      notesContainer.append('<div class="note-block">No notes for this day.</div>');
+      $('#remove-note').hide();
+    }
+  }
+
+  $('#positive').click(() => saveNoteToCalendar('Positive', 'green'));
+  $('#triggered').click(() => saveNoteToCalendar('Triggered', 'red'));
+  $('#relapsed').click(() => {
+    saveNoteToCalendar('Relapsed', 'orange');
+    resetTimer();
+  });
+  $('#add-note').click(() => {
+    $('#note-popup').hide();
+    $('#note-input-popup').show();
+  });
+  $('#remove-note').click(() => removeNoteFromCalendar());
+  $('#view-day').click(() => viewDayJournal());
+
+  $('#save-note').click(() => {
+    const selectedDate = $('#note-popup').data('date');
+    const customNote = $('#custom-note').val();
+    if (customNote) {
+      addNoteToDate(selectedDate, {
+        title: customNote,
+        color: 'blue',
+        note: customNote
+      });
+      $('#custom-note').val(''); // Clear the input
+      $('#note-input-popup').hide();
+      displayCalendar($('#startDate').val());
+    }
+  });
+
+  $('#cancel-note').click(() => {
+    $('#note-input-popup').hide();
+  });
+
+  function saveNoteToCalendar(note, color) {
+    const selectedDate = $('#note-popup').data('date');
+    addNoteToDate(selectedDate, {
+      title: note,
+      color: color,
+      note: note
+    });
+    $('#note-popup').hide();
+    displayCalendar($('#startDate').val());
+  }
+
+  function addNoteToDate(date, note) {
+    if (!storedNotes[date]) {
+      storedNotes[date] = [];
+    }
+    storedNotes[date].push(note);
+    localStorage.setItem('notes', JSON.stringify(storedNotes));
+  }
+
+  function removeNoteFromCalendar() {
+    const selectedDate = $('#note-popup').data('date');
+    delete storedNotes[selectedDate];
+    localStorage.setItem('notes', JSON.stringify(storedNotes));
+    $('#note-popup').hide();
+    displayCalendar($('#startDate').val());
+  }
+
+  function viewDayJournal() {
+    const selectedDate = $('#note-popup').data('date');
+    const journalEntries = storedNotes[selectedDate]
+      ? storedNotes[selectedDate].map(note => `<div class="note-block">${note.note}</div>`).join('')
+      : '<div class="note-block">No notes for this day.</div>';
+    $('#day-journal').html(journalEntries);
+    $('#note-popup').hide();
+    $('#journal-popup').show();
+  }
+
+  $('#journal-popup button').click(() => {
+    $('#journal-popup').hide();
+  });
+
+  $('#popup-overlay, #note-popup-overlay').on('click', () => {
+    $('#note-popup').hide();
+    $('#journal-popup').hide();
+    $('#note-input-popup').hide();
+  });
+
+  let timerInterval;
+
+  function startTimer(commitTime) {
+    clearInterval(timerInterval); // Clear any existing timer
+
+    timerInterval = setInterval(function() {
+      const currentTime = new Date();
+      const timeDiff = currentTime - new Date(commitTime);
+      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+      $('#timeCounter').text(`Time since commit: ${days}d ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`);
+    }, 1000);
+  }
+
+  function resetTimer() {
+    clearInterval(timerInterval);
+    const newCommitTime = new Date();
+    localStorage.setItem('commitTime', newCommitTime.toISOString());
+    startTimer(newCommitTime);
+  }
+
+  function formatTime(unit) {
+    return unit < 10 ? '0' + unit : unit;
+  }
+
+  const loginButton = document.getElementById('login');
+  loginButton.addEventListener('click', () => {
+    netlifyIdentity.open();
+  });
+
+  netlifyIdentity.on('login', user => {
+    console.log('User logged in:', user);
+    netlifyIdentity.close();
+  });
+
+  netlifyIdentity.on('logout', () => {
+    console.log('User logged out');
+    window.location.href = 'index.html';
+  });
+
+  // Initialize the calendar with stored events
+  const commitTime = localStorage.getItem('commitTime');
+  if (commitTime) {
+    displayCalendar(moment(commitTime).format('YYYY-MM-DD'));
+    startTimer(new Date(commitTime));
+  } else {
+    displayCalendar(new Date());
+  }
 }
 
-function viewResourceDetail(index) {
-  const resource = resources[index];
-  localStorage.setItem('selectedResource', JSON.stringify(resource));
-  window.location.href = 'resource-detail.html';
-}
-
-document.getElementById('resourceForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-  const dependency = document.getElementById('dependency').value;
-  const postalCode = document.getElementById('postalCode').value;
-
-  console.log("Selected Dependency:", dependency);
-  console.log("Entered Postal Code:", postalCode);
-
-  const filteredResources = resources.filter(resource => 
-    resource.dependency_category.includes(dependency) &&
-    resource.postal_code.startsWith(postalCode)
-  );
-
-  console.log("Filtered Resources:", filteredResources);
-
-  renderResourceList(filteredResources);
-});
-}
 $(document).ready(initializePage);
 $(window).on('pageshow', initializePage);
-document.addEventListener('DOMContentLoaded', () => {
-  renderResourceList(resources);
-});
+
+window.onunload = function() {
+  // When the user navigates away from the page, save the current state to localStorage
+  var state = getCurrentState();
+  localStorage.setItem('savedState', JSON.stringify(state));
+};
+
+function getCurrentState() {
+  // Get the current state of your application
+  // This will depend on your specific application
+}
