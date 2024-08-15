f071e0bbe9f63651e1954814ab5cd9c8a19c2e41
periagoge1980 <123590443+periagoge1980@users.noreply.github.com>
2024-08-14 20:52:35 -0400

    Update getSupabaseKey.js

--- netlify/functions/getSupabaseKey.js
+++ netlify/functions/getSupabaseKey.js
@@ -1,6 +1,15 @@
 exports.handler = async function(event, context) {
+  const supabaseKey = process.env.SUPABASE_KEY;
+
+  if (!supabaseKey) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'No API key found' })
+    };
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ key: process.env.SUPABASE_KEY }),
+    body: JSON.stringify({ key: supabaseKey })
   };
 };
